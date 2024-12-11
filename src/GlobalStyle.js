bd3ad390d9ea5911d8b9ed502a0768b4a32db54c
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-11 13:08:33 +0100

    Initial commit

---
+++ src/GlobalStyle.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+html {
+box-sizing: border-box;
+}
+
+*, ::before, ::after {
+box-sizing: inherit;
+}
+
+body{
+background-color: ${({ theme }) => theme.colors.background};
+font-family: ${({ theme }) => theme.text.textMainFont};
+min-height: 100vh;
+display: flex;
+flex-direction: column;
+}
+`;
---
+++ src/theme.js
@@ -0,0 +1,13 @@
+export const theme = {
+  colors: {
+    background: "#FBFBFE",
+  },
+
+  text: {
+    textMainFont: "'Inter', sans-serif",
+  },
+
+  breakpoints: {
+    mobile: 767,
+  },
+};
