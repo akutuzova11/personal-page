474878cd19f4ca16f999fddb42ce919c114dc8d9
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-13 12:54:28 +0100

    add Portfolio to the App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { About } from "./feature/About";
 import { mySkills, toLearn } from "./metrics";
 import { Section } from "./feature/Section";
 import { Footer } from "./feature/Footer";
+import { Portfolio } from "./feature/Portfolio";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <About />
       <Section title="My skillset includes 🛠️" listOfMetrics={mySkills}/>
       <Section title="What I want to learn next 🚀" listOfMetrics={toLearn}/>
+      <Portfolio />
       <Footer/>
     </>
   );
