f4fb42078fcc25729b82390d5db798f6d1252032
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-13 12:53:48 +0100

    Add loading, success, and error state handling for repositories data

--- src/feature/Portfolio/index.js
+++ src/feature/Portfolio/index.js
@@ -1,19 +1,45 @@
+import { socialMedia } from "../../socialMedia";
+import { Loading } from "./Loading";
+import { ProjectsList } from "./ProjectsList";
 import { Article, Wrapper, Header, Paragraph } from "./styled";
+import { useRepositories } from "./useRepositories";
+import { Error } from "./Error";
+import { IconLink } from "../IconLinks";
 
 export const Portfolio = () => {
+  const data = useRepositories();
+
+  const getState = () => {
+    if (data.state === "loading") {
+      return (
+        <div>
+          {" "}
+          <Loading />
+        </div>
+      );
+    } else if (data.state === "success") {
+      return <ProjectsList projects={data.repositoriesData} />;
+    } else if (data.state === "error") {
+      return <Error />;
+    }
+  };
+  const gitHubSocial = socialMedia.find(
+    (socialMedia) => socialMedia.name === "GitHub"
+  );
   return (
     <>
       <Article>
         <Wrapper>
           <IconLink
             $portfolio
-            href={xyz}
-            title={xyz}
+            href={gitHubSocial.url}
+            title={gitHubSocial.name}
             target="_blank"
             rel="noopener noreferrer"
           ></IconLink>
           <Header>Portfolio</Header>
           <Paragraph>My recent projects</Paragraph>
+          {getState()}
         </Wrapper>
       </Article>
     </>
