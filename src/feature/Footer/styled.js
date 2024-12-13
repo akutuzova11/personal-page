73d9a8f48b7a1264ae5530112ab672af29f1dd90
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-13 11:17:52 +0100

    style footer component

---
+++ src/feature/Footer/styled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const FooterStyled = styled.footer`
+  background-color: #f3f4f6; // Example background
+  padding: 20px;
+  text-align: center;
+`;
+
+export const Icons = styled.ul`
+  display: flex;
+  gap: 16px;
+  list-style: none;
+  padding: 0;
+  margin: 16px 0 0;
+  justify-content: center;
+`;
