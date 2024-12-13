087958ac29758894c5bfc1c8b92c5dca7f3ccee5
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-13 11:17:35 +0100

    Create Footer component with contact and social media links

---
+++ src/feature/Footer/index.js
@@ -0,0 +1,35 @@
+import { email } from "../../email";
+import { PersonalInformation } from "../PersonalInformation";
+import { socialMedia } from "../../socialMedia";
+import { IconStyled } from "../IconLinks/index";
+import { FooterStyled, Icons } from "./styled";
+
+export const Footer = () => {
+  return (
+    <>
+      <FooterStyled>
+        <PersonalInformation
+          caption="LET'S TALK"
+          isFooter={true}
+          title={email}
+          description="Let’s make your ideas come to life! If you’re working on a website, dashboard, or mobile app and need some help, don’t hesitate to get in touch. I’d love to collaborate."
+        />
+        <Icons>
+          {socialMedia.map(({ name, url, Icon }) => (
+            <li key={name}>
+              <IconStyled
+                as="a"
+                href={url}
+                title={name}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Icon />
+              </IconStyled>
+            </li>
+          ))}
+        </Icons>
+      </FooterStyled>
+    </>
+  );
+};
