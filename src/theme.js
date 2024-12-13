2e94214c89c4516c2d20f931d87be04bde8d3fd8
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-13 11:16:49 +0100

    add black & white colors to the theme

--- src/theme.js
+++ src/theme.js
@@ -2,8 +2,8 @@ export const theme = {
   colors: {
     background: "#FBFBFE",
     textSecondary: "#6E7E91",
-    textBlack: "#252525",
-    textWhite: "#FFFFFF", 
+    black: "#252525",
+    white: "#FFFFFF", 
     buttonColor: "#0366D6",
     colorActive: "#2188FF"
   },
