1de7823b163e3b6ed033fcebc8d4b8c6745e1449
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-13 11:16:27 +0100

    add sections and footer to the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { About } from "./feature/About";
+import { mySkills, toLearn } from "./metrics";
+import { Section } from "./feature/Section";
+import { Footer } from "./feature/Footer";
 
 function App() {
   return (
     <>
       <About />
+      <Section title="My skillset includes 🛠️" listOfMetrics={mySkills}/>
+      <Section title="What I want to learn next 🚀" listOfMetrics={toLearn}/>
+      <Footer/>
     </>
   );
 }
