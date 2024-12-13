bcc07fa92312f79a48b7477019fd4538e3b8d242
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-13 11:19:06 +0100

    Create Section component to display title and list of metrics

---
+++ src/feature/Section/index.js
@@ -0,0 +1,10 @@
+import { List } from "../List";
+import { Divider, Header, SectionStyled } from "./styled";
+
+export const Section = ({ title, listOfMetrics }) => (
+  <SectionStyled>
+    <Header>{title}</Header>
+    <Divider />
+    <List listOfMetrics={listOfMetrics} />
+  </SectionStyled>
+);
