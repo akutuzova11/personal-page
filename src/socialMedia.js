1426bed4b41d0b5d5adb4cb0aba1312702ed9d34
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-13 11:16:03 +0100

    Add social media links with icons for GitHub, Facebook, and LinkedIn

---
+++ src/socialMedia.js
@@ -0,0 +1,22 @@
+import { ReactComponent as GitHubIcon } from "./images/Github_black.svg";
+import { ReactComponent as FacebookIcon } from "./images/Facebook_black.svg";
+import { ReactComponent as LinkedInIcon } from "./images/LinkedIN_black.svg";
+import { IconStyled } from "./feature/IconLinks";
+
+export const socialMedia = [
+  {
+    name: "GitHub",
+    url: "https://github.com/akutuzova11",
+    Icon: GitHubIcon,
+  },
+  {
+    name: "Facebook",
+    url: "https://www.facebook.com/angie2637/",
+    Icon: FacebookIcon,
+  },
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/angelina-kutuzova/",
+    Icon: LinkedInIcon,
+  },
+];
