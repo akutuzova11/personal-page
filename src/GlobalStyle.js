0ebe0e428a688a285ce3dde31deab82742bf8780
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-14 10:48:59 +0100

    update font styling

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -11,7 +11,7 @@ box-sizing: inherit;
 
 body{
 background-color: ${({ theme }) => theme.colors.background};
-font-family: ${({ theme }) => theme.fonts.textMainFont};
+font-family: ${({ theme }) => theme.fonts.main};
 min-height: 100vh;
 display: flex;
 flex-direction: column;
