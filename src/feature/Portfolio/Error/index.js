33816378a28f9c9540217efe76fcd160b1ca9787
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-14 10:51:42 +0100

    implement light and dark icons for error handling

--- src/feature/Portfolio/Error/index.js
+++ src/feature/Portfolio/Error/index.js
@@ -1,11 +1,17 @@
+import { useSelector } from "react-redux";
 import { Button } from "../../Button";
 import { repositories } from "../repositories";
 import {Wrapper, Subwrapper, Header, Paragraph} from "./styled"
+import { selectIsDark } from "../../ModeSwitch/themeSlice";
+import {ReactComponent as ErrorIconLight} from "../../../images/errorLight.svg";
+import {ReactComponent as ErrorIconDark} from "../../../images/errorDark.svg";
 
 export const Error = () => {
+    const isDark = useSelector(selectIsDark);
   return (
     <Wrapper>
       <Subwrapper>
+        {isDark ? <ErrorIconDark/> : <ErrorIconLight/>}
         <Header>Ooops! Something went wrong</Header>
         <Paragraph>
           Sorry, failed to load GitHub projects. You can check them directly on
