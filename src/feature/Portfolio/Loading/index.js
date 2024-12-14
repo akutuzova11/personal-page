915f287991999a736aedd59140a62b9892b0f659
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-14 10:52:23 +0100

    implement light and dark icons for loading mode

--- src/feature/Portfolio/Loading/index.js
+++ src/feature/Portfolio/Loading/index.js
@@ -1,10 +1,15 @@
-import { Paragraph, RotatingIcon } from "./styled";
+import { useSelector } from "react-redux";
+import { Paragraph, RotatingIcon, LoadingIconDark, LoadingIconLight } from "./styled";
+import { selectIsDark } from "../../ModeSwitch/themeSlice";
 
 export const Loading = () => {
+  const isDark = useSelector(selectIsDark);
   return (
     <>
       <Paragraph>Please wait, projects are being loaded</Paragraph>
-      <RotatingIcon></RotatingIcon>
+      <RotatingIcon>
+        {isDark ? <LoadingIconDark /> : <LoadingIconLight />}
+      </RotatingIcon>
     </>
   );
 };
