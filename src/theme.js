da984fb761b2bf652eff0fadd08f3069c86b9d58
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-14 10:49:49 +0100

    include light and dark themes

--- src/theme.js
+++ src/theme.js
@@ -1,11 +1,30 @@
-export const theme = {
+export const themeLight = {
   colors: {
     background: "#FBFBFE",
     textSecondary: "#6E7E91",
     black: "#252525",
-    white: "#FFFFFF", 
+    white: "#FFFFFF",
     buttonColor: "#0366D6",
-    colorActive: "#2188FF"
+    colorActive: "#2188FF",
+  },
+
+  fonts: {
+    main: "'Inter', sans-serif",
+  },
+
+  breakpoints: {
+    mobile: 767,
+  },
+};
+
+export const themeDark = {
+  colors: {
+    background: "#FBFBFE",
+    textSecondary: "#6E7E91",
+    black: "#252525",
+    white: "#FFFFFF",
+    buttonColor: "#0366D6",
+    colorActive: "#2188FF",
   },
 
   fonts: {
