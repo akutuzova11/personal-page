b58a052b10457116e1f5b430dd5c9c81db5987ed
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-16 11:39:46 +0100

    add styled components for footer layout

--- src/feature/Footer/styled.js
+++ src/feature/Footer/styled.js
@@ -1,16 +1,15 @@
 import styled from "styled-components";
 
 export const FooterStyled = styled.footer`
-  background-color: #f3f4f6; // Example background
-  padding: 20px;
-  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-top: 120px;
 `;
 
 export const Icons = styled.ul`
   display: flex;
-  gap: 16px;
+  gap: 24px;
   list-style: none;
   padding: 0;
-  margin: 16px 0 0;
-  justify-content: center;
 `;
