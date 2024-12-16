24dc7ca12f91612e4c7d7494d9ea13901f889fb1
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-16 11:35:44 +0100

    implement error icons

--- src/feature/Portfolio/Error/index.js
+++ src/feature/Portfolio/Error/index.js
@@ -1,26 +1,30 @@
 import { useSelector } from "react-redux";
 import { Button } from "../../Button";
 import { repositories } from "../repositories";
-import {Wrapper, Subwrapper, Header, Paragraph} from "./styled"
-import { selectIsDark } from "../../ModeSwitch/themeSlice";
-import {ReactComponent as ErrorIconLight} from "../../../images/errorLight.svg";
-import {ReactComponent as ErrorIconDark} from "../../../images/errorDark.svg";
+import { Wrapper, Subwrapper, Header, Paragraph, GitHubIcon } from "./styled";
+import { selectIsDark } from "../../ToggleMode/themeSlice";
+import { ReactComponent as ErrorIconLight } from "../../../images/errorLight.svg";
+import { ReactComponent as ErrorIconDark } from "../../../images/errorDark.svg";
 
 export const Error = () => {
-    const isDark = useSelector(selectIsDark);
+  const isDark = useSelector(selectIsDark);
   return (
     <Wrapper>
       <Subwrapper>
-        {isDark ? <ErrorIconDark/> : <ErrorIconLight/>}
-        <Header>Ooops! Something went wrong</Header>
-        <Paragraph>
-          Sorry, failed to load GitHub projects. You can check them directly on
-          GitHub
-        </Paragraph>
+        {isDark ? <ErrorIconLight /> : <ErrorIconDark />}
+        <Header>Ooops! Something went wrong...</Header>
+        <Paragraph>Sorry, failed to load GitHub projects.</Paragraph>
+        <Paragraph>You can check them directly on GitHub</Paragraph>
+
+        <Button
+          href={repositories}
+          $gitHubRedirect
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Go to GitHub
+        </Button>
       </Subwrapper>
-      <Button href={repositories} target="_blank" rel="noopener noreferrer">
-        Go to GitHub
-      </Button>
     </Wrapper>
   );
 };
