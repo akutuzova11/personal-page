c30556e7bfdf9b419412ba5d5c453467eef9077f
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-16 11:33:45 +0100

    display project details

--- src/feature/Portfolio/ProjectsList/index.js
+++ src/feature/Portfolio/ProjectsList/index.js
@@ -1,15 +1,38 @@
-import { List, Item, Header, Description, Wrapper, Paragraph, Link } from "./styled";
-
+import {
+  List,
+  Item,
+  Header,
+  Description,
+  Wrapper,
+  Paragraph,
+  Link,
+} from "./styled";
 
 export const ProjectsList = ({ projects }) => (
   <List>
     {projects.map((project) => (
       <Item key={project.id}>
-        <Header></Header>
-        <Description></Description>
+        <Header>{project.name}</Header>
+        <Description>{project.description}</Description>
         <Wrapper>
-          <Paragraph>Demo: </Paragraph>
-          <Paragraph>Code: </Paragraph>
+          <Paragraph>
+            Demo:{" "}
+            <Link
+              href={project.homepage}
+              target="_blank"
+              rel="noopener noreferrer"
+            />
+            {project.homepage}
+          </Paragraph>
+          <Paragraph>
+            Code:{" "}
+            <Link
+              href={project.html_url}
+              target="_blank"
+              rel="noopener noreferrer"
+            />
+            {project.html_url}
+          </Paragraph>
         </Wrapper>
       </Item>
     ))}
