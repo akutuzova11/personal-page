9e0c34907203f509fc1f25c25fb9725b30cddc56
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-16 11:39:24 +0100

    update styling for list component

--- src/feature/List/styled.js
+++ src/feature/List/styled.js
@@ -6,8 +6,26 @@ export const ListStyled = styled.ul`
   column-gap: 115px;
   row-gap: 8px;
   font-size: 18px;
+  font-weight: 400;
+  margin: 0;
 `;
 
 export const Item = styled.li`
-color: ${({theme})=> theme.colors.textSecondary}
+position: relative; 
+  padding-left: 25px; 
+  color: ${({ theme }) => theme.colors.paragraphColor};
+  list-style: none;
+  line-height: 1.4;
+
+  &::before {
+  content: "";
+  position: absolute;
+  top: 50%;
+  left: 0;
+  transform: translateY(-50%);
+  height: 9px;
+  width: 9px;
+  background-color: ${({theme})=> theme.colors.sectionDot};
+  border-radius: 50%;
+  }
 `;
