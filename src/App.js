dafd8e23a3a658dbe4537775b536f9a5be305eae
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-16 11:44:28 +0100

    implement theme toggle

--- src/App.js
+++ src/App.js
@@ -3,16 +3,29 @@ import { mySkills, toLearn } from "./metrics";
 import { Section } from "./feature/Section";
 import { Footer } from "./feature/Footer";
 import { Portfolio } from "./feature/Portfolio";
+import { useSelector } from "react-redux";
+import { themeDark, themeLight } from "./theme";
+import { ThemeProvider } from "styled-components";
+import { selectIsDark } from "./feature/ToggleMode/themeSlice";
+import { ToggleMode } from "./feature/ToggleMode";
+import { Container } from "./common/Container";
+import { GlobalStyle } from "./GlobalStyle";
 
 function App() {
+  const isDark = useSelector(selectIsDark);
+
   return (
-    <>
-      <About />
-      <Section title="My skillset includes 🛠️" listOfMetrics={mySkills}/>
-      <Section title="What I want to learn next 🚀" listOfMetrics={toLearn}/>
-      <Portfolio />
-      <Footer/>
-    </>
+    <ThemeProvider theme={isDark ? themeDark : themeLight}>
+      <GlobalStyle/>
+      <Container>
+        <ToggleMode />
+        <About />
+        <Section title="My skillset includes 🛠️" listOfMetrics={mySkills} />
+        <Section title="What I want to learn next 🚀" listOfMetrics={toLearn} />
+        <Portfolio />
+        <Footer />
+      </Container>
+    </ThemeProvider>
   );
 }
 
