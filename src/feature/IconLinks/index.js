69f60c6d52969a50860ad8baee66c8c68b580e12
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-17 16:29:27 +0100

    add styling for the icon link

--- src/feature/IconLinks/index.js
+++ src/feature/IconLinks/index.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { ReactComponent as GitHubIcon } from "../../images/github.svg";
 
 export const IconStyled = styled.div`
   width: 48px;
@@ -9,9 +10,27 @@ export const IconStyled = styled.div`
   align-items: center;
   justify-content: center;
   transition: background-color 0.3s;
-
-  &:hover {
+  path {
+    fill: ${({ theme }) => theme.colors.footerIconColor};
   }
 `;
 
-export const IconLink = styled.a``;
+export const PortfolioLink = styled.a`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  transition: background-color 0.3s;
+
+  svg path {
+    fill: ${({ theme }) => theme.colors.portfolioIconColor};
+  }
+`;
+
+export const IconLink = ({ href, title, target, rel }) => (
+  <PortfolioLink href={href} title={title} target={target} rel={rel}>
+    <GitHubIcon />
+  </PortfolioLink>
+);
