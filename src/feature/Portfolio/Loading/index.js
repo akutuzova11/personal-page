34e52c61924edeb888f4eb1a88c40dc7ce74481e
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-17 16:27:41 +0100

    replace react fragment with Wrapper styling component

--- src/feature/Portfolio/Loading/index.js
+++ src/feature/Portfolio/Loading/index.js
@@ -1,15 +1,15 @@
 import { useSelector } from "react-redux";
-import { Paragraph, RotatingIcon, LoadingIconDark, LoadingIconLight } from "./styled";
+import { Paragraph, RotatingIcon, LoadingIconDark, LoadingIconLight, Wrapper } from "./styled";
 import { selectIsDark } from "../../ToggleMode/themeSlice";
 
 export const Loading = () => {
   const isDark = useSelector(selectIsDark);
   return (
-    <>
+    <Wrapper>
       <Paragraph>Please wait, projects are being loaded</Paragraph>
       <RotatingIcon>
         {isDark ? <LoadingIconDark /> : <LoadingIconLight />}
       </RotatingIcon>
-    </>
+    </Wrapper>
   );
 };
