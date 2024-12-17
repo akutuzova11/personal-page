bff16451d6bf5bf19e6b9df6af3b5600419284cc
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-17 16:31:30 +0100

    update margins

--- src/common/Container/index.js
+++ src/common/Container/index.js
@@ -1,11 +1,11 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-max-width: 1920px;
-margin: 119px 352px;
+  max-width: 1216px;
+  margin: 119px auto 109px auto;
+  word-wrap: normal;
 
-@media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
-
-margin: 34px 16px;
-}
-`
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    margin: 34px 16px;
+  }
+`;
