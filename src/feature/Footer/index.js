d5568744f0244691d7ccd6c457432702d74ae521
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-19 15:42:17 +0100

    refactor Footer component for improved readability and structure

--- src/feature/Footer/index.js
+++ src/feature/Footer/index.js
@@ -4,32 +4,28 @@ import { socialMedia } from "../../socialMedia";
 import { IconStyled } from "../IconLinks/index";
 import { FooterStyled, Icons } from "./styled";
 
-export const Footer = () => {
-  return (
-    <>
-      <FooterStyled>
-        <PersonalInformation
-          caption="LET'S TALK"
-          isFooter={true}
-          title={<a href={`mailto:${email}`}>{email}</a>}
-          description="Let’s make your ideas come to life! If you’re working on a website, dashboard, or mobile app and need some help, don’t hesitate to get in touch. I’d love to collaborate."
-        />
-        <Icons>
-          {socialMedia.map(({ name, url, Icon }) => (
-            <li key={name}>
-              <IconStyled
-                as="a"
-                href={url}
-                title={name}
-                target="_blank"
-                rel="noreferrer"
-              >
-                <Icon />
-              </IconStyled>
-            </li>
-          ))}
-        </Icons>
-      </FooterStyled>
-    </>
-  );
-};
+export const Footer = () => (
+  <FooterStyled>
+    <PersonalInformation
+      caption="LET'S TALK"
+      isFooter={true}
+      title={<a href={`mailto:${email}`}>{email}</a>}
+      description="Let’s make your ideas come to life! If you’re working on a website, dashboard, or mobile app and need some help, don’t hesitate to get in touch. I’d love to collaborate."
+    />
+    <Icons>
+      {socialMedia.map(({ name, url, Icon }) => (
+        <li key={name}>
+          <IconStyled
+            as="a"
+            href={url}
+            title={name}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Icon />
+          </IconStyled>
+        </li>
+      ))}
+    </Icons>
+  </FooterStyled>
+);
