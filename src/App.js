f7e6632a89599a6f7bfa153289c06bdce2f56c84
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-19 15:18:17 +0100

    implement useMemo and reorder imports

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import { About } from "./feature/About";
-import { mySkills, toLearn } from "./metrics";
-import { Section } from "./feature/Section";
-import { Footer } from "./feature/Footer";
-import { Portfolio } from "./feature/Portfolio";
+import React, { useMemo } from "react";
 import { useSelector } from "react-redux";
-import { themeDark, themeLight } from "./theme";
 import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./GlobalStyle";
+import { themeDark, themeLight } from "./theme";
 import { selectIsDark } from "./feature/ToggleMode/themeSlice";
 import { ToggleMode } from "./feature/ToggleMode";
+import { About } from "./feature/About";
+import { Section } from "./feature/Section";
+import { mySkills, toLearn } from "./metrics";
+import { Footer } from "./feature/Footer";
+import { Portfolio } from "./feature/Portfolio";
 import { Container } from "./common/Container";
-import { GlobalStyle } from "./GlobalStyle";
 
 function App() {
   const isDark = useSelector(selectIsDark);
+  const theme = useMemo(() => (isDark ? themeDark : themeLight), [isDark]);
 
   return (
-    <ThemeProvider theme={isDark ? themeDark : themeLight}>
-      <GlobalStyle/>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <Container>
         <ToggleMode />
         <About />
