4d3530b326d6af29a1812ec5854e6e97d8adf099
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-19 17:16:03 +0100

    fix container centering and padding for tablet and mobile views

--- src/common/Container/index.js
+++ src/common/Container/index.js
@@ -2,10 +2,17 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   max-width: 1216px;
-  margin: 119px 109px;
+  margin: 119px auto;
   word-wrap: normal;
+  
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
+    margin: 34px auto;
+    padding: 0 16px; 
+  }
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
-    margin: 34px 16px;
+    margin: 34px auto;
+    padding: 0 16px; 
   }
 `;
