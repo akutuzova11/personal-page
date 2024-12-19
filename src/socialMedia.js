f5c92cd4f3af16f2c100f12b24a083b7235e3552
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-19 15:24:08 +0100

    reorder alphabetically

--- src/socialMedia.js
+++ src/socialMedia.js
@@ -1,17 +1,17 @@
-import { ReactComponent as GitHubIcon } from "./images/github.svg";
 import { ReactComponent as FacebookIcon } from "./images/facebook.svg";
+import { ReactComponent as GitHubIcon } from "./images/github.svg";
 import { ReactComponent as LinkedInIcon } from "./images/linkedin.svg";
 
 export const socialMedia = [
   {
-    name: "GitHub",
-    url: "https://github.com/akutuzova11",
-    Icon: GitHubIcon,
-  },
-  {
     name: "Facebook",
     url: "https://www.facebook.com/angie2637/",
     Icon: FacebookIcon,
+  },
+  {
+    name: "GitHub",
+    url: "https://github.com/akutuzova11",
+    Icon: GitHubIcon,
   },
   {
     name: "LinkedIn",
