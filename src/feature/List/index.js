1c0d4c0786aed147c4d718a5b008990fdee5bf01
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-19 15:55:52 +0100

    refactor List component for responsive layout

--- src/feature/List/index.js
+++ src/feature/List/index.js
@@ -2,8 +2,8 @@ import { ListStyled, Item } from "./styled";
 
 export const List = ({ listOfMetrics }) => (
   <ListStyled>
-    {listOfMetrics.map((content, value) => (
-      <Item key={value}>{content}</Item>
+    {listOfMetrics.map((item, index) => (
+      <Item key={index}>{item}</Item>
     ))}
   </ListStyled>
 );
