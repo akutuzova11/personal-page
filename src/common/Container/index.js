a146ae2799ab72f8f3a33ff8bbb70295b4045e6a
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-12-22 11:19:25 +0100

    remove redundant mobile media query for consistent tablet responsiveness

--- src/common/Container/index.js
+++ src/common/Container/index.js
@@ -4,15 +4,9 @@ export const Container = styled.div`
   max-width: 1216px;
   margin: 119px auto;
   word-wrap: normal;
-  
 
   @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
     margin: 34px auto;
-    padding: 0 16px; 
-  }
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
-    margin: 34px auto;
-    padding: 0 16px; 
+    padding: 0 16px;
   }
 `;
